a61183145cad0b2ce027e5b71df0cb61d8c3c0a3
Jayden Winkler <jaydenw2@illinois.edu>
2020-04-28 21:53:12 -0500

    Implemented it so that a random song will play when the app is opened. Implemented music seek bar? Double check this.
    Note to self - figure out emulator problem so I can test this stuff myself

--- app/src/main/java/com/example/tron/MainActivity.java
+++ app/src/main/java/com/example/tron/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tron;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,10 +15,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         configureStartButton();
         configureSettingsButton();
+        //picks a song from the jukebox and plays it when the app opens. song loops.
+        ChooseRandomSongActivity songInstance = new ChooseRandomSongActivity();
+        MediaPlayer song = songInstance.chooseRandomSong();
+        song.setLooping(true);
+        song.start();
     }
 
     private void configureStartButton() {
-        Button startButton = (Button) findViewById(R.id.startGameButton);
+        Button startButton = findViewById(R.id.startGameButton);
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void configureSettingsButton() {
-        Button settingsButton = (Button) findViewById(R.id.settingsButton);
+        Button settingsButton = findViewById(R.id.settingsButton);
         settingsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
---
+++ app/src/main/java/com/example/tron/SetMusicSliderActivity.java
@@ -0,0 +1,49 @@
+package com.example.tron;
+
+import android.app.Activity;
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+
+public class SetMusicSliderActivity extends Activity implements OnSeekBarChangeListener {
+    private SeekBar musicSeekBar;
+    private MediaPlayer mediaPlayer;
+    private AudioManager audioManager;
+    private int volume = 0;
+
+    /**
+     * This onCreate initialized the seekbar for Music.
+     * reference code: https://mycodeandlife.wordpress.com/2012/07/13/android-seekbar-to-set-the-volume/
+     * @param inputBundle input bundle.
+     */
+    protected void onCreate(Bundle inputBundle) {
+        super.onCreate(inputBundle);
+        setContentView(R.layout.activity_settings);
+
+        musicSeekBar = findViewById(R.id.musicSeekBar);
+        ChooseRandomSongActivity song = new ChooseRandomSongActivity();
+        mediaPlayer = song.chooseRandomSong();
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        mediaPlayer.start();
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        musicSeekBar.setMax(maxVolume);
+        musicSeekBar.setProgress(currentVolume);
+        musicSeekBar.setOnSeekBarChangeListener(this);
+    }
+
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean arg2) {
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+        volume = progress;
+    }
+    public void onStartTrackingTouch(SeekBar arg0) {
+        // stuff here
+    }
+    public void onStopTrackingTouch(SeekBar arg0) {
+        // stuff here
+    }
+}
